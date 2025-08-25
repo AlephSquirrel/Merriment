6f9d8e4317bf61741a2cd4516e9fd12ea1f5e8f8
AlephSquirrel <33697577+AlephSquirrel@users.noreply.github.com>
2025-08-24 21:16:43 -0500

    Add files via upload

---
+++ Merriment.py
@@ -0,0 +1,177 @@
+'''
+    Interpreter for the language Merriment
+    Author: AlephSquirrel
+    Version: 0.1
+'''
+
+import sys, re, argparse
+
+def err_message(e):
+    sys.stderr.write('Error: ' + e)
+    sys.exit(1)
+
+class Codebox:
+    def __init__(self, name, width, height, entry_col, code):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.entry_col = entry_col
+        self.code = code
+
+def read_code(filename):
+    '''
+        Reads code from {filename}.merry.
+        Returns a dictionary mapping commands to Codebox objects.
+    '''
+    try:
+        f = open(filename + ".merry", 'r')
+        lines = f.readlines()
+        f.close()
+    except IOError:
+        err_message(f"Can't read file {filename}.merry")
+    
+    codeboxes = dict()
+    in_codebox = False
+    
+    for line_num, line in enumerate(lines):
+        line = line.rstrip('\n')
+        if in_codebox:
+            if len(line) != codebox_width:
+                err_message(f"Incorrect width on line {line_num + 1}")
+            codebox_lines += 1
+            if codebox_lines == 2:
+                # Parse codebox name
+                name_match = re.fullmatch("# *(.*?) *#", line)
+                if name_match:
+                    name = name_match.group(1)
+                else:
+                    err_message(f"Bad codebox name on line {line_num + 1}")
+            elif codebox_lines == 3:
+                # Parse codebox entry line
+                entry_match = re.fullmatch("#=*v=*#", line)
+                if entry_match:
+                    entry_col = entry_match.group(0).find('v') - 1
+                else:
+                    err_message(f"Bad entry point on line {line_num + 1}")
+            elif re.fullmatch("#*", line):
+                # Parse bottom border of codebox
+                codeboxes[name[:1]] = Codebox(name, codebox_width - 2, len(code), entry_col, code)
+                in_codebox = False
+            else:
+                # Parse any lines inside of the codebox
+                row_match = re.fullmatch("#(.*)#", line)
+                if row_match:
+                    code.append(row_match.group(1))
+                else:
+                    err_message(f"Bad codebox row on line {line_num + 1}")
+        else:
+            if re.fullmatch("#+", line):
+                # Top border of codebox
+                in_codebox = True
+                codebox_width = len(line)
+                codebox_lines = 1
+                code = []
+            else:
+                # Parse import statement
+                import_match = re.fullmatch("{(.*)}", line)
+                if import_match: codeboxes.update(read_code(import_match.group(1)))
+    return codeboxes
+
+def run_code(codeboxes, stack, vstack, name = ''):
+    if name not in codeboxes:
+        if name == '':
+            err_message("No main codebox found")
+        else:
+            err_message(f"Command not found: {name}")
+    codebox = codeboxes[name]
+    x,y = codebox.entry_col, 0
+    vx,vy = 0,1
+    string_mode = False
+    
+    while True:
+        # Test for out-of-bounds
+        if (x < 0) or (x >= codebox.width) or (y < 0) or (y >= codebox.height):
+            err_message("Out of bounds")
+        
+        command = codebox.code[y][x]
+        if string_mode:
+            if command == '"':
+                string_mode = False
+            else:
+                stack.append(ord(command))
+        elif command == ' ':
+            "NOP"
+        elif command.isdigit():
+            # Digits
+            stack.append(int(command))
+        elif command == '↊':
+            # Push 10
+            stack.append(10)
+        elif command == '↋':
+            # Push 11
+            stack.append(11)
+        elif command in "+-*,":
+            # Arithmetic operations
+            a = stack.pop()
+            b = stack.pop()
+            if command == '+':
+                stack.append(a + b)
+            elif command == '-':
+                stack.append(b - a)
+            elif command == '*':
+                stack.append(a * b)
+            elif command == ',':
+                stack.append(b // a)
+        elif command == '`':
+            # Is positive?
+            a = stack.pop()
+            stack.append(int(a > 0))
+        elif command == ':':
+            # Duplicate
+            a = stack.pop()
+            for _ in range(2): stack.append(a)
+        elif command == '.':
+            # Pop
+            stack.pop()
+        elif command == '~':
+            # Swap
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a)
+            stack.append(b)
+        elif command == '{':
+            # Move value from velocity stack to data stack
+            stack.append(vstack.pop())
+        elif command == '}':
+            # Move value from data stack to velocity stack
+            vstack.append(stack.pop())
+        elif command == '@':
+            # Return
+            return
+        elif command == '"':
+            string_mode = True
+        elif command == 'i':
+            # Input 1 character from stdin.
+            # If at EOF, return -1.
+            char = sys.stdin.read(1)
+            stack.append(ord(char) if char else -1)
+        elif command == 'o':
+            # Output character
+            print(end=chr(stack.pop()))
+        else:
+            # Push velocity to velocity stack, execute command, pop new velocity from velocity stack.
+            vstack.append(vx)
+            vstack.append(vy)
+            run_code(codeboxes, stack, vstack, command)
+            vy = vstack.pop()
+            vx = vstack.pop()
+        # Update IP position
+        x += vx; y += vy
+
+parser = argparse.ArgumentParser(description='Run a Merriment program')
+parser.add_argument('progname', metavar='progname', type=str,
+    help='the filename of the Merriment program to run')
+args = parser.parse_args()
+
+codeboxes = read_code(args.progname)
+run_code(codeboxes, [], [])
---
+++ README.md
@@ -0,0 +1,75 @@
+# Merriment
+Merriment is a 2D stack-based programming language, inspired by Befunge and other similar languages. The main thing that sets Merriment apart is its extensibility. Code is written in codeboxes, which can be called from other codeboxes. This gives code a lot more reusability compared to other similar 2D languages.
+
+## Codeboxes
+Here is an example of a codebox:
+```
+#######
+# foo #
+#v====#
+#>123@#
+#######
+```
+The name of this codebox is `foo`. (Leading and trailing spaces in the name are ignored.) Execution starts at the `v` in the line below the name and continues downward. Like plenty of other 2D languages, the `>` command redirects the instruction pointer toward the right. Then, 1, 2, and 3 are pushed onto the stack. Finally, we hit the `@` symbol, which returns control to whatever codebox called `foo`.
+
+How do we call this codebox? Using the first character in its name. So, to call `foo`, we use the `f` command. Suppose we have this codebox in the same file:
+```
+########
+#      #
+#v=====#
+#>f++o@#
+########
+```
+This codebox has an empty name, making it the "main codebox". When the program is run, this codebox will be called. First, it calls `foo`, which pushes 1, 2, and 3. Then these values are added together, resulting in 6. The `o` command prints ASCII character 6 (ACK), and finally the `@` command causes the program to terminate.
+
+## Data Stack and Velocity Stack
+There are two stacks that are used to hold data. Both can hold arbitrarily sized integers. One of them is the *data stack*, as seen in the previous example. The other is the *velocity stack*. When a codebox is called, the velocity of the instruction pointer is pushed to the velocity stack. First the x component, then the y component. After returning from the call, the components are popped again in the reverse order.
+
+If the called codebox does nothing with the velocity stack, this means the IP velocity remains unchanged after the call. The `{` and `}` commands can be used to interact with the velocity stack. In fact, this is how the arrow commands are defined. If you want, you could implement the mirrors (`/` and `\`) from ><>, or make up your own commands that interact with the IP velocity in new ways.
+
+The velocity stack can also be used as an extra place to store data. You can see it being used that way in some of the codeboxes in the standard library.
+
+## Imports
+To import another file, you just need an import statement on one line like this: `{file}` This will look for a file called `file.merry` and import everything from it.
+
+For most programs, you probably want to import `{stdlib}`, since that will allow you to use arrows and a few other functions that are useful for general programming.
+
+## Commands
+### Base commands
+|Command|Function|
+|--|--|
+|0-9|Push value to the data stack|
+|↊|Push 10|
+|↋|Push 11|
+|+|Pop a, pop b, push a+b|
+|-|Pop a, pop b, push b-a|
+|*|Pop a, pop b, push a*b|
+|,|Pop a, pop b, push floor(b/a) (Errors on division by 0)|
+|`|Is positive: pop value, then push 1 if it's positive, or 0 otherwise|
+|:|Duplicate|
+|.|Pop|
+|~|Swap|
+|{|Move value from velocity stack to data stack|
+|}|Move value from data stack to velocity stack|
+|@|Return|
+|"|Toggle string mode|
+|i|Input character (-1 on EOF)|
+|o|Output character|
+### Commands in {stdlib}
+|Command|Function|
+|--|--|
+|>|Right: Set velocity to 1,0|
+|<|Left: Set velocity to -1,0|
+|v|Down: Set velocity to 0,1|
+|^|Up: Set velocity to 0,-1|
+|%|Modulo: Pop a, pop b, push b%a|
+|(|Less Than: Pop a, pop b, then push 1 if b<a or 0 otherwise|
+|)|Greater Than: Pop a, pop b, then push 1 if b>a or 0 otherwise|
+|=|Equals: Pop a, pop b, then push 1 if b=a or 0 otherwise|
+|_|Horizontal If: Pop value, then go right if it's positive, or left if it's non-positive.|
+|\||Vertical If: Pop value, then go down if it's positive, or up if it's non-positive.|
+|b|boost: Pop value and multiply IP velocity by it.|
+|p|print: Print a string from the stack terminated by a non-positive value.|
+|r|rot: a b c -> b c a|
+|g|get: Pop n, then push the nth item from the top of the stack (0-indexed). If n is negative, get the 0th item.|
+|s|set: Pop n, pop x, then set the nth item from the top of the stack to x (0-indexed). If n is negative, set the 0th item.|
